2e9240996bd79789038661ea5c060c823a81a904
VIVI-XX <395425496@qq.com>
2019-05-14 21:09:46 +0800

    Temperature and Rate convertion
    Signed-off-by: VIVI-XX <395425496@qq.com>

--- app/src/main/java/com/example/tinkpad/temperatureconvertion/MainActivity.java
+++ app/src/main/java/com/example/tinkpad/temperatureconvertion/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.tinkpad.temperatureconvertion;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +9,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
-import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView out;
@@ -22,9 +21,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edit =findViewById(R.id.inp);
-        out =findViewById(R.id.txtout);
-        out1 =findViewById(R.id.txtout1);
+        edit = findViewById(R.id.inp);
+        out = findViewById(R.id.txtout);
+        out1 = findViewById(R.id.txtout1);
         btn = findViewById(R.id.btn);
         btn.setOnClickListener(this);
 
@@ -33,15 +32,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Log.i("main","onClick msg…");
-        String str=edit.getText().toString();
-        String str1=out1.getText().toString();
-        float num=Float.parseFloat(str);
-        num = num*9/5+32 ;
-        BigDecimal bd=new BigDecimal(num);
-        double num1= bd.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+        Log.i("main", "onClick msg…");
+        String str = edit.getText().toString();
+        String str1 = out1.getText().toString();
+        float num = Float.parseFloat(str);
+        num = num * 9 / 5 + 32;
+        BigDecimal bd = new BigDecimal(num);
+        double num1 = bd.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
 
-        out1.setText(str1 + num1 +"℉");
+        out1.setText(str1 + num1 + "℉");
 
     }
 }
